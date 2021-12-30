d7dbad0c32ffa82817dddc787187eb4d2b043765
Rahul Kurian Jacob <82926759+coderRKJ@users.noreply.github.com>
2021-12-31 00:14:26 +0530

    Add Additional Screens and Routing Logic
    Added dart files for home, splash and new screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:example_app/home_screen.dart';
 import 'package:example_app/new_screen.dart';
+import 'package:example_app/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,116 +16,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Counter Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      drawer: Drawer(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(20), bottomRight: Radius.circular(20)),
-        ),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(20),
-                ),
-                color: Theme.of(context).primaryColor, //Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Theme.of(context).secondaryHeaderColor, //Colors.white,
-                  fontSize: 36,
-                ),
-              ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Messages'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            const ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          padding: const EdgeInsets.all(8),
-          shrinkWrap: true,
-          children: <Widget>[
-            Center(
-              child: Text(
-                'You have clicked the button this many times:',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-            ),
-            Center(
-              child: Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) {
-                    return const Screen();
-                  },
-                ));
-              },
-              title: const Text("Test Tap"),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      routes: {
+        "new_page": (context) => const Screen(),
+        "home": (context) =>
+            const MyHomePage(title: 'Flutter Counter Home Page'),
+      },
+      home: const SplashScreen(),
     );
   }
 }
--- lib/new_screen.dart
+++ lib/new_screen.dart
@@ -6,7 +6,15 @@ class Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("New Screen"),
+      ),
+      body: Center(
+        child: TextButton(
+          child: const Text("Go Back"),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
     );
   }
 }
