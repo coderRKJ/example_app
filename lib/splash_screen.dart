a8574696522260570c66da96764e3baf7aa76897
Rahul Kurian Jacob <82926759+coderRKJ@users.noreply.github.com>
2022-01-04 23:55:20 +0530

    Add SplashScreen Widget
    Warning! SplashScreen Package is NOT Null Safe.
    Flutter run requires "--no-sound-null-safety" argument to compile.

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+// class Splashscreen extends StatelessWidget {
+//   const Splashscreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Splash Screen"),
+//       ),
+//       body: Center(
+//         child: TextButton(
+//           child: const Text("Go to Home Screen"),
+//           onPressed: () {
+//             Navigator.of(context).pushReplacementNamed("home");
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Splashscreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Splash Screen"),
-      ),
-      body: Center(
-        child: TextButton(
-          child: const Text("Go to Home Screen"),
-          onPressed: () {
-            Navigator.of(context).pushReplacementNamed("home");
-          },
+    return SplashScreen(
+        seconds: 4,
+        navigateAfterSeconds: "home",
+        title: const Text(
+          'MockGen App',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
-      ),
-    );
+        loadingText: const Text("Loading..."),
+        image: Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Theme.of(context).backgroundColor,
+        styleTextUnderTheLoader: const TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.blueAccent);
   }
 }
